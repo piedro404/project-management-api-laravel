259dced813d4efb8bcdbebd307f3cf8ff7479ed6
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-08-01 12:04:08 -0300

    feat: :seedling: Add Favicon

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -4,18 +4,35 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <meta name="description" content="SwaggerUI" />
-
     <title>Project Management API</title>
 
-    {{-- Icon --}}
-    <link rel="shortcut icon" href={{ url('favicon.png') }} type="image/x-icon">
+    {{-- Icon and Swagger --}}
+    <script type="text/javascript">
+        window.onload = () => {
+            const protocol = window.location.protocol;
+            const host = window.location.host;
+            const baseUrl = `${protocol}//${host}`;
+            const faviconUrl = `${baseUrl}/favicon.png`;
+
+            const linkElement = document.createElement('link');
+            linkElement.rel = 'shortcut icon';
+            linkElement.href = faviconUrl;
+            linkElement.type = 'image/x-icon';
+
+            document.head.appendChild(linkElement);
+
+            const openApiUrl = `${baseUrl}/docs/openapi.yaml`;
+            window.ui = SwaggerUIBundle({
+                url: openApiUrl,
+                dom_id: '#swagger-ui',
+            });
+        };
+    </script>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui.css" />
@@ -24,19 +41,6 @@
 <body class="antialiased">
     <div id="swagger-ui"></div>
     <script src="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-bundle.js" crossorigin></script>
-    <script>
-        window.onload = () => {
-            const protocol = window.location.protocol;
-            const host = window.location.host;
-            const baseUrl = `${protocol}//${host}`;
-            const openApiUrl = `${baseUrl}/docs/openapi.yaml`;
-
-            window.ui = SwaggerUIBundle({
-                url: openApiUrl,
-                dom_id: '#swagger-ui',
-            });
-        };
-    </script>
 </body>
 
 </html>
